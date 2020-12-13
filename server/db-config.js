c0c5ea5738e4f462672364a5688031e6245e4642
sallyhu-seas <sallyhu714@gmail.com>
2020-12-13 17:28:05 -0500

    update pw

--- server/db-config.js
+++ server/db-config.js
@@ -1,5 +1,5 @@
 module.exports = {
   user: "admin",
-  password: "550Group7BE$T!",
+  password: "550Group7BE$T!!",
   connectString: "cis550proj.cfot25rjb5mg.us-east-1.rds.amazonaws.com/jeopardy"
 };
